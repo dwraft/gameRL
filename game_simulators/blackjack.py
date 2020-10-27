ddc4ad62b20a5b5dd2f350b5ee4828306ac2cdde
Daniel Wrafter <dwrafter@mit.edu>
2020-10-27 13:26:30 -0400

    Directory setup

---
+++ game_simulators/blackjack.py
@@ -0,0 +1,16 @@
+"""
+Modeled largely after
+https://github.com/openai/gym/blob/master/gym/envs/toy_text/blackjack.py
+I implemented the class from scratch in case we want to make modifications in the future
+"""
+import gym
+from gym import spaces
+
+class BlackjackCustomEnv(gym.Env):
+
+    def __init__(self):
+        pass
+
+    def seed(self, seed=None):
+        pass
+
